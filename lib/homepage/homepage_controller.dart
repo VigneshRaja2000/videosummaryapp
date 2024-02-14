c74361c1efb18d28d14b78a1870f0e732945d278
svsvijay <50706060+svsvijay@users.noreply.github.com>
2024-02-14 16:27:55 +0530

    update package

--- lib/homepage/homepage_controller.dart
+++ lib/homepage/homepage_controller.dart
@@ -1,107 +1,40 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video/constants/path.dart';
 import 'package:video/homepage/homepage_variables.dart';
-import 'package:flutter_tts/flutter_tts.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class HomeController extends GetxController with HomeVariables {
   init() async {}
   convert() async {
     // Get.toNamed(AppPaths.summary);
-    await fetchCaptionTracks();
-    // await main();
-    // speakText(url.text);
+    await getCaptions('8jPQjjsBbIc');
   }
 
-  Future<void> fetchCaptionTracks() async {
-    final String apiKey =
-        'AIzaSyDX4XhyKC2UmOXSRzxIcxgJUlDnPr8NwCE'; // Replace with your actual YouTube Data API key
-    final String videoId =
-        'd2om_PGtwWY'; // Replace with the actual ID of the video
-    String _captionTracksInfo = 'Caption tracks info will appear here';
-    final Uri url = Uri.parse(
-        'https://www.googleapis.com/youtube/v3/captions/AUieDaYJusRvxR9ydp-sTa7M2hmKcr9CtalHNNz0UiJg5x9DWrk?tfmt=srt&key=$apiKey');
-
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      // Parse the JSON response
-
-      _captionTracksInfo = response.body;
-      print(response.body);
-      // Contains info about available caption tracks
-    } else {
-      _captionTracksInfo = 'Failed to load caption tracks';
-    }
-  }
-
-  main() async {
-    var url = Uri.parse("https://api.openai.com/v1/chat/completions");
-    var apiKey = "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";
-    var headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer $apiKey",
-    };
-    var json = [
-      {
-        'text': 'a few years ago i broke into my own',
-        'start': 13.28,
-        'duration': 5.12
-      }
-    ];
-    var data = {
-      "model": "gpt-4-1106-preview",
-      "messages": [
-        {
-          "role": "user",
-          "content":
-              "i have a json string with subtitles ,Clean up this subtitles and summaries this " +
-                  jsonEncode(json)
-        }
-      ]
-    };
-
-    var response = await http.post(
-      url,
-      headers: headers,
-      body: jsonEncode(data),
-    );
-
-    if (response.statusCode == 200) {
-      print(response.body);
-      // Handle response
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-  }
-
-  // Future<void> speakText(text) async {
-  //   //   try {
-  //   //     await flutterTts.speak(text);
-  //   //   } catch (e) {
-  //   //     // Handle the exception here
-  //   //     print("Error during text-to-speech: $e");
-  //   //     // You can choose to handle the exception in a different way, like showing a dialog or logging it.
-  //   //   }
-  //   // }
-
-  Future<void> speakText(String text) async {
+  Future<void> getCaptions(String videoId) async {
+    var yt = YoutubeExplode();
     try {
-      FlutterTts flutterTts = FlutterTts();
-      await flutterTts
-          .setPitch(10); // Increase pitch (range usually between 0.5 and 2.0)
-      await flutterTts.setVoice(
-          {"name": "en-us-x-sfg#male_1-local"}); // Change voice to male
+      // Get the manifest that contains all available closed caption tracks
+      var manifest = await yt.videos.closedCaptions.getManifest(videoId);
+      print(manifest.tracks);
+      if (manifest.tracks.isNotEmpty) {
+        // Just for example, we'll take the first track
+        var trackInfo = manifest.tracks.first;
 
-      await flutterTts.setLanguage("en-US");
+        // Get the actual closed caption track which contains all the captions
+        var track = await yt.videos.closedCaptions.get(trackInfo);
 
-      // Speak the text
-      await flutterTts.speak(text);
+        // Print all captions
+        for (var caption in track.captions) {
+          print('${caption.text} (${caption.offset})');
+        }
+      } else {
+        print('No captions found for video ID: $videoId');
+      }
     } catch (e) {
-      // Handle the exception here
-      print("Error during text-to-speech: $e");
-      // You can choose to handle the exception in a different way, like showing a dialog or logging it.
+      print('Failed to fetch captions: $e');
+    } finally {
+      yt.close();
     }
   }
 }
--- lib/homepage/homepage_variables.dart
+++ lib/homepage/homepage_variables.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 
 mixin HomeVariables {
   TextEditingController url = TextEditingController();
-  FlutterTts flutterTts = FlutterTts();
 }
